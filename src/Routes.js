74f50d4f2328257d0c7aabf69b340805d4bf87e3
Mohamed Aslam <mohamedaslamcodes@gmail.com>
2024-08-04 23:07:41 +0530

    file-change

--- src/Routes.js
+++ src/Routes.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/User/Home.jsx";
 import { AuthProvider } from "./services/auth.js";
 import AdminRoute from "./routes/admin-route.jsx";
-import AdminHome from "./pages/Admin/AdminHome.jsx";
+import AdminHome from "./pages/admin/admin-home.jsx";
 import TabSwitcher from "./pages/TabSwitcher.jsx";
 import { SavedArticlesProvider } from "./services/SaveArticleContext.jsx";
 
